9683cec3f8fa3dd15982dab8a81a816d918651dd
blafond <blafond@redhat.com>
2021-07-19 14:26:42 -0500

    Working JUnit test and working jbang test based on hibernate-reactive.version:1.0.0.CR7

--- hibernate-reactive-core/src/test/java/org/hibernate/reactive/Issue886_Test.java
+++ hibernate-reactive-core/src/test/java/org/hibernate/reactive/Issue886_Test.java
@@ -18,11 +18,14 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.cfg.Configuration;
+import org.hibernate.proxy.HibernateProxy;
+import org.hibernate.reactive.provider.Settings;
 
 import org.junit.After;
 import org.junit.Test;
 
 import io.vertx.ext.unit.TestContext;
+import org.assertj.core.api.Assertions;
 
 public class Issue886_Test extends BaseReactiveTest {
 
@@ -31,6 +34,10 @@ public class Issue886_Test extends BaseReactiveTest {
 		Configuration configuration = super.constructConfiguration();
 		configuration.addAnnotatedClass( SampleEntity.class );
 		configuration.addAnnotatedClass( SampleJoinEntity.class );
+
+		configuration.setProperty( Settings.SHOW_SQL, System.getProperty(Settings.SHOW_SQL, "true") );
+		configuration.setProperty( Settings.FORMAT_SQL, System.getProperty(Settings.FORMAT_SQL, "false") );
+		configuration.setProperty( Settings.HIGHLIGHT_SQL, System.getProperty(Settings.HIGHLIGHT_SQL, "true") );
 		return configuration;
 	}
 
@@ -40,37 +47,53 @@ public class Issue886_Test extends BaseReactiveTest {
 	}
 
 	@Test
-	public void testInsertAndSelect(TestContext context) {
+	public void testONE(TestContext context) {
+		final String ORIGINAL_FIELD = "I am the ORIGINAL field value";
+		final String UPDATED_FIELD = "I am the UPDATED field value";
+
 		SampleEntity sampleEntity = new SampleEntity();
-		sampleEntity.sampleField = "test";
+		sampleEntity.sampleField = ORIGINAL_FIELD;
 		SampleJoinEntity sampleJoinEntity = new SampleJoinEntity();
 		sampleJoinEntity.sampleEntity = sampleEntity;
 
-		test(
-				context,
-				getMutinySessionFactory()
-						.withTransaction( (s, t) -> s.persistAll( sampleEntity, sampleJoinEntity ) )
-						.chain( () -> getMutinySessionFactory()
-								.withTransaction( (session, transaction) -> session.find(
-										SampleJoinEntity.class, sampleJoinEntity.id )
-										.invoke( context::assertNotNull )
-								)
-								.chain( result -> getMutinySessionFactory()
-										.withStatelessTransaction(
-												(s3, transaction1) -> {
-													result.sampleEntity.sampleField = "updatedTest";
-													return s3.withTransaction(
-															tx -> s3.update( result.sampleEntity )
-													);
-												} ) )
+		test( context, getMutinySessionFactory().withTransaction( (s, t) -> s
+				.persist( sampleEntity ) // PERSIST primary entity
+				.call( s::flush )
+				.chain( () -> s.find( SampleEntity.class, sampleEntity.id ) )
+				.invoke( found -> context.assertEquals( sampleEntity, found ) ) )
+				.chain( () -> getMutinySessionFactory().withTransaction( (session, transaction) -> session
+						.persist( sampleJoinEntity ) // PERSIST associated/joined entity
+						.call( session::flush )
+						.chain( () -> session.find( SampleJoinEntity.class, sampleJoinEntity.id ) )
+						.invoke( found -> context.assertEquals( sampleJoinEntity, found )) )
+				)
+				.chain( result -> getMutinySessionFactory().withStatelessTransaction(
+						(s, t) -> {
+							context.assertFalse( result instanceof HibernateProxy );
+							result.sampleEntity.sampleField = UPDATED_FIELD;
+							return s.withTransaction( tx -> s.update( result.sampleEntity )
+									.chain( () -> s.refresh( result.sampleEntity )));
+						} )
+				)
+				.chain( () -> getMutinySessionFactory()
+						.withSession( session -> session.find(
+								SampleEntity.class, sampleEntity.id )
+								.chain( se -> session.fetch( se ) )
+								.invoke( result -> {
+									context.assertFalse( result instanceof HibernateProxy );
+									Assertions.assertThat( result ).isNotInstanceOf( HibernateProxy.class );
+									context.assertEquals( UPDATED_FIELD, result.sampleField );
+								} )
 						)
-//						.chain( () -> getMutinySessionFactory()
-//								.withSession( session -> session.find(
-//										SampleJoinEntity.class, sampleJoinEntity.id )
-//										.invoke( result -> context.assertEquals( "updatedTest", result.sampleEntity.sampleField )
-//										)
-//								)
-//						)
+				)
+				.chain( () -> getMutinySessionFactory()
+						.withSession( session -> session.find(
+								SampleJoinEntity.class, sampleJoinEntity.id )
+								.chain( () -> session.fetch( sampleJoinEntity) )
+								.invoke( found -> context.assertEquals( sampleJoinEntity, found )
+								) )
+						)
+
 		);
 	}
 
@@ -83,6 +106,20 @@ public class Issue886_Test extends BaseReactiveTest {
 
 		@Column(name = "sample_field")
 		public String sampleField;
+
+		public void setSampleField(String sampleField) {
+			this.sampleField = sampleField;
+		}
+
+		public String getSampleField() {
+			return this.sampleField;
+		}
+
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + " ID: " + id + " sampleField: " + sampleField;
+		}
+
 	}
 
 	@Entity(name = "SampleJoinEntity")
@@ -95,5 +132,14 @@ public class Issue886_Test extends BaseReactiveTest {
 		@ManyToOne(fetch = FetchType.LAZY)
 		@JoinColumn(name = "sample_entity_id", referencedColumnName = "id")
 		public SampleEntity sampleEntity;
+
+		public SampleEntity getSampleEntity() {
+			return this.sampleEntity;
+		}
+
+		@Override
+		public String toString() {
+			return getClass().getSimpleName() + " ID: " + id + " sampleEntity: " + sampleEntity;
+		}
 	}
 }
